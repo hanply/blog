54e1e109c5423c50f21ad50902390b354b46753e
lycaizi <214304879@qq.com>
2016-07-12 20:09:22 +0800

    2

--- test.php
+++ test.php
@@ -2,6 +2,6 @@
 	include 'func.php';
 
 
-	// $sql = 'INSERT INTO cate (pid , cate_name) VALUES(0 ,"HTML")';
+	// $sql = 'INSERT INTO cate (pid , cate_name) VALUES(0 ,"HTML")'12;
 
 	var_dump(DB($sql));
